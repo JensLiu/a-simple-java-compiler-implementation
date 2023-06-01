5d6db680aa7f60bd2d644471733b6f96f489b486
Jens Liu <jensliu29@outlook.de>
2023-06-02 00:23:30 +0800

    added enum to string table

--- Token.cpp
+++ Token.cpp
@@ -3,15 +3,14 @@
 //
 
 #include "Token.h"
+#include <ostream>
 
-#include <utility>
-
-Token::Token(const Token::TokenType& tokenType, const std::string& lexeme) {
+Token::Token(const Token::TokenType &tokenType, const std::string &lexeme) {
     this->tokenType = tokenType;
-    this->lexeme = std::move(lexeme);
+    this->lexeme = lexeme;
 }
 
-Token::Token(const Token::TokenType& tokenType) {
+Token::Token(const Token::TokenType &tokenType) {
     this->tokenType = tokenType;
     this->lexeme = "";
 }
@@ -22,4 +21,88 @@ Token::TokenType Token::getTokenType() const {
 
 const std::string &Token::getLexeme() const {
     return this->lexeme;
+}
+std::ostream& operator<<(std::ostream& os, const Token& token) {
+    return os << "<" << Token::tokenTypeAsString(token.getTokenType()) << "(" << token.getTokenType() << ")" << ", " << token.lexeme << ">";
+}
+
+const std::unordered_map<Token::TokenType, std::string> Token::tokenName = {
+        {Token::TokenType::INVALID_TOKEN, "INVALID_TOKEN"},
+        {Token::TokenType::IF, "if"},
+        {Token::TokenType::ELSE, "else"},
+        {Token::TokenType::WHILE, "while"},
+        {Token::TokenType::RETURN, "return"},
+        {Token::TokenType::CLASS, "class"},
+        {Token::TokenType::EXTENDS, "extends"},
+        {Token::TokenType::IMPLEMENTS, "implements"},
+        {Token::TokenType::NEW, "new"},
+        {Token::TokenType::THIS, "this"},
+        {Token::TokenType::SUPER, "super"},
+        {Token::TokenType::PUBLIC, "public"},
+        {Token::TokenType::PRIVATE, "private"},
+        {Token::TokenType::PROTECTED, "protected"},
+        {Token::TokenType::STATIC, "static"},
+        {Token::TokenType::PACKAGE, "package"},
+        {Token::TokenType::INT, "int"},
+        {Token::TokenType::FLOAT, "float"},
+        {Token::TokenType::BOOL, "bool"},
+        {Token::TokenType::CHAR, "char"},
+        {Token::TokenType::STRING, "string"},
+        {Token::TokenType::VOID, "void"},
+        {Token::TokenType::IDENTIFIER, "id"},
+        {Token::TokenType::INTEGER_LITERAL, "int_literal"},
+        {Token::TokenType::FLOAT_LITERAL, "float_literal"},
+        {Token::TokenType::CHAR_LITERAL, "char_literal"},
+        {Token::TokenType::STRING_LITERAL, "string_literal"},
+        {Token::TokenType::BOOL_LITERAL, "bool_literal"},
+        {Token::TokenType::PLUS, "+"},
+        {Token::TokenType::MINUS, "-"},
+        {Token::TokenType::STAR, "*"},
+        {Token::TokenType::SLASH, "/"},
+        {Token::TokenType::PERCENT, "%"},
+        {Token::TokenType::CARET, "^"},
+        {Token::TokenType::TILDE, "~"},
+        {Token::TokenType::AMPERSAND, "&"},
+        {Token::TokenType::PIPE, "|"},
+        {Token::TokenType::EQUALS, "="},
+        {Token::TokenType::NOT_EQUALS, "!="},
+        {Token::TokenType::LESS_THAN, "<"},
+        {Token::TokenType::GREATER_THAN, ">"},
+        {Token::TokenType::LESS_THAN_OR_EQUAL, "<="},
+        {Token::TokenType::GREATER_THAN_OR_EQUAL, ">="},
+        {Token::TokenType::LOGICAL_AND, "&"},
+        {Token::TokenType::LOGICAL_OR, "|"},
+        {Token::TokenType::LOGICAL_NOT, "!"},
+        {Token::TokenType::INCREMENT, "++"},
+        {Token::TokenType::DECREMENT, "--"},
+        {Token::TokenType::ASSIGNMENT, "="},
+        {Token::TokenType::PLUS_ASSIGNMENT, "+="},
+        {Token::TokenType::MINUS_ASSIGNMENT, "-="},
+        {Token::TokenType::STAR_ASSIGNMENT, "*="},
+        {Token::TokenType::SLASH_ASSIGNMENT, "/="},
+        {Token::TokenType::PERCENT_ASSIGNMENT, "%="},
+        {Token::TokenType::CARET_ASSIGNMENT, "^="},
+//        {Token::TokenType::TILDE_ASSIGNMENT, "TILDE_ASSIGNMENT"},
+        {Token::TokenType::AMPERSAND_ASSIGNMENT, "&="},
+        {Token::TokenType::PIPE_ASSIGNMENT, "|="},
+        {Token::TokenType::LEFT_PAREN, "("},
+        {Token::TokenType::RIGHT_PAREN, ")"},
+        {Token::TokenType::LEFT_BRACE, "{"},
+        {Token::TokenType::RIGHT_BRACE, "}"},
+        {Token::TokenType::LEFT_BRACKET, "["},
+        {Token::TokenType::RIGHT_BRACKET, "]"},
+        {Token::TokenType::SEMICOLON, ";"},
+        {Token::TokenType::COMMA, ","},
+        {Token::TokenType::DOT, "."},
+//        {Token::TokenType::COLON, "COLON"},
+//        {Token::TokenType::QUESTION_MARK, "QUESTION_MARK"},
+        {Token::TokenType::END_OF_FILE, "EOF"},
+        {Token::TokenType::ERROR, "ERROR"},
+        {Token::TokenType::WHITESPACE, "WHITESPACE"},
+        {Token::TokenType::NULL_T, "null"},
+        {Token::TokenType::EPSILON, "ϵ"}
+};
+
+std::string Token::tokenTypeAsString(const Token::TokenType &tokenType) {
+    return tokenName.at(tokenType);
 }
