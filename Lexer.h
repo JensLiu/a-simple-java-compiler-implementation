7e980bf3b2078d92b8de026acd2ceef2aff83492
Jens Liu <jensliu29@outlook.de>
2023-06-02 00:24:00 +0800

    added error report

--- Lexer.h
+++ Lexer.h
@@ -14,7 +14,7 @@ public:
     static bool isDigit(char c);
     static bool isLetter(char c);
     static bool isWhitespace(char c);
-    static bool mightBeOperator(char c);
+    static bool isOperator(char c);
     static const int TOKEN_BUFFER_SIZE = 1024;
 
 public:
@@ -52,6 +52,23 @@ public:
     Token handleDelimiter();
 
     static bool isDelimiter(char c);
+};
+
+class LexicalError : public std::runtime_error {
+private:
+    int line;
+    int column;
+    std::string message;
+public:
+    LexicalError(const std::string& error, int line, int column)
+        : std::runtime_error(error), line(line + 1), column(column + 1) {
+        this->message = error + " at line " + std::to_string(line + 1)  + " at column " + std::to_string(column + 1);
+    }
+    [[nodiscard]] const char * what () const noexcept override {
+        return message.c_str();
+    }
+    [[nodiscard]] int getLine() const { return this->line; }
+    [[nodiscard]] int getColumn() const { return this->column; }
 };
 
 
